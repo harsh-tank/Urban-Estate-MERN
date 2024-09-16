dc10c975b43a2c64d206938fdeee6c8de428463d
Harsh Tank <harshptank2212@gmail.com>
2024-09-15 20:01:01 -0400

    initial dev

---
+++ Backend/index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+dotenv.config();
+
+const connectDB = async() => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URL);
+    console.log("DB connected Successfully");
+  } catch (error) {
+    console.log(error);
+  }
+}
+connectDB();
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000!');
+  });
