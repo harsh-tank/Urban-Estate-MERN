f421fff6a6f9d0eb6fe7ed014a585ad8e18e7102
Harsh Tank <harshptank2212@gmail.com>
2024-09-18 19:32:56 -0400

    sign-in functionality added

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,5 +1,9 @@
 import User from '../models/user.model.js';
+import dotenv from 'dotenv';
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { errorHandler } from '../utils/error.js';
+dotenv.config();
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -17,6 +21,22 @@ export const signup = async (req, res, next) => {
     }
   };
 
-export const signin = (req, res, next) => {
+export const signin = async(req, res, next) => {
+    const { email, password } = req.body;
+    
+    try {
+      const user = await User.findOne({email});
+      if(!user){
+        return next(errorHandler(404, 'User not found!'))
+      }
+      const validpass = bcryptjs.compareSync(password, user.password);
+      if(!validpass)return next(errorHandler(401, 'Wrong credentials!'));
+      const token = jwt.sign({id:user._id}, process.env.JWT_SECRET);
+      const {password: pass, ...rest} = user._doc;
+      return res.cookie('access_token', token, {httpOnly:true}).status(200).json(rest);
+    } catch (error) {
+      console.log(error)
+      next(error);
+    }
     
 };
--- Frontend/src/pages/SignIn.jsx
+++ Frontend/src/pages/SignIn.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
 
 function SignIn() {
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+  const handleChange = (e) =>{
+    setFormData({
+      ...formData,
+     [e.target.id]: e.target.value,
+    })
+  }
+  const handleSubmit = async(e) =>{
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await axios.post('/api/auth/signin',{
+        ...formData
+      })
+      console.log(res);
+      if(res.success===false){
+        setLoading(false);
+        setError(res.message);
+        return
+      }
+      setLoading(false);
+      setError(null);
+      localStorage.setItem('token',res.data.token);
+      navigate('/');
+
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  }
   return (
-    <div>SignIn</div>
+    <div className='max-w-lg mx-auto p-3'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+        <input type='text' placeholder='email' id='email' className='focus:outline-none border p-3 rounded-lg' onChange={handleChange}></input>
+        <input type='password' placeholder='password' id='password' className='focus:outline-none border p-3 rounded-lg' onChange={handleChange}></input>
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:75'> {loading?`Loading...`:`Sign In`} </button>
+      </form>
+      <div className='mt-3'><p>New User ? <span className='text-sky-600'><Link to={'/sign-up'}>Sign Up</Link></span> </p></div>
+      
+    </div>
   )
 }
 
